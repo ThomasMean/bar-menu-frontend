3029e6c6291a7ddcabfc74bc015d1f6e3daa1315
Thomas Mean <mail@thomasmean.co.uk>
2020-02-28 19:08:58 +0000

    Make call to backend, return simple data and print to screen

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
-import Item from './Item';
+import React from 'react';
+import ItemComponent from './Item';
+import Item from '../models/Item';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag'
 
-class ItemListComponent extends Component {
-    render() {
-        const items = [
-            {
-                "id": 1,
-                "name": "Beer",
-                "price": 260
-            },
-            {
-                "id": 2,
-                "name": "Lager",
-                "price": 280
-            }
-        ]
-        return (
-            <div>{items.map(item => <Item key={item.id} item={item} />)}</div>
-        )
+
+const QUERY = gql`
+{
+    items {
+        id
+        name
+        price
     }
 }
+`
+
+function Content() {
+    const { loading, error, data } = useQuery(QUERY);
+
+    if (loading) return <div>Loading..</div>
+    if (error) return <div>Error...</div>
+    if (!data) return <div>No Data..</div>
+
+    const items: Item[] = data.items;
+    return (<div>{items.map((item: Item) => <ItemComponent key={item.id} item={item} />)}</div>)
+}
+
+const ItemListComponent = () => <Content />
+
 
 export default ItemListComponent
